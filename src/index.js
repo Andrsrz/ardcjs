fab1da63c3b8ed0759ff788befa27c00624b3881
Andres Ruiz <andrsruiz@gmail.com>
2020-10-22 16:06:47 -0500

    Create Init method to add app div into document

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-export const init = (selector, component) => {
-	const APP = document.querySelector(selector);
+export const Init = () => {
+	const APP = document.createElement('div');
+	APP.id = 'app';
+	document.body.append(APP);
+};
+
+export const CreateComponent = (component) => {
+	const APP_ID = '#app';
+	const APP = document.querySelector(APP_ID);
 	const NEW_ELEMENT = document.createElement(component.type);
-	const NEW_TEXT_CONTENT = document.createTextContent(component.template);
+	const NEW_TEXT_CONTENT = document.createTextNode(component.template);
 
 	NEW_ELEMENT.append(NEW_TEXT_CONTENT);
 	APP.append(NEW_ELEMENT);
