9fa5cd0c1213293ef86a2424a4382c8f5dc5e7ef
Andres Ruiz <andrsruiz@gmail.com>
2020-10-22 17:17:49 -0500

    Initialize snabbdom in index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { init } from 'snabbdom/init';
+const patch = init([]);
+
 export const Init = () => {
 	const APP = document.createElement('div');
 	APP.id = 'app';
@@ -7,9 +10,5 @@ export const Init = () => {
 export const CreateComponent = (component) => {
 	const APP_ID = '#app';
 	const APP = document.querySelector(APP_ID);
-	const NEW_ELEMENT = document.createElement(component.type);
-	const NEW_TEXT_CONTENT = document.createTextNode(component.template);
-
-	NEW_ELEMENT.append(NEW_TEXT_CONTENT);
-	APP.append(NEW_ELEMENT);
+	patch(APP, component.template);
 };
